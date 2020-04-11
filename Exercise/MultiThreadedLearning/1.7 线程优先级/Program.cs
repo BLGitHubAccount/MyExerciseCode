e7b67d9c2a414349d0e58afac159fdb6342f3585
baoliangjin <mrbaoliangjin@gmail.com>
2020-04-11 22:35:30 +0800

    commit date 2020-04-11

---
+++ Exercise/MultiThreadedLearning/1.7 线程优先级/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+using System.Diagnostics;
+
+     
+
+namespace _1._7_线程优先级
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"Current Thread  priority :{Thread.CurrentThread.Priority} ");
+            WriteLine($"runing on all cores available");
+            RunThreads();
+            WriteLine($"runing on single core");
+            Process.GetCurrentProcess().ProcessorAffinity = new IntPtr(1);
+            RunThreads();
+
+            ReadKey();
+        }
+
+        static void RunThreads()
+        {
+            var sample = new ThreadSample();
+            var threadOne = new Thread(sample.CountNumbers);
+            threadOne.Name = "ThreadOne";
+             
+            var threadTwo = new Thread(sample.CountNumbers);
+            threadTwo.Name = "ThreadTwo";
+
+            threadOne.Priority = ThreadPriority.Highest;
+            threadTwo.Priority = ThreadPriority.Lowest;
+            threadOne.Start();
+            threadTwo.Start();
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            sample.Stop();
+        }
+
+    }
+    public class ThreadSample
+    {
+        private bool _isStoped = false;
+        public void Stop()
+        {
+            _isStoped = true;
+        }
+        public void CountNumbers()
+        {
+            long counter = 0;
+            while (!_isStoped)
+            {
+                counter++;
+            }
+            WriteLine($"{Thread.CurrentThread.Name} with"+$"{Thread.CurrentThread.Priority,11} priority"+$"has a count={counter,13:N0}");
+        }
+    }
+}
---
+++ Exercise/MultiThreadedLearning/1.8前台线程和后台线程/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace _1._8前台线程和后台线程
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start run");
+            SampleThread sample1 = new SampleThread(3);
+            SampleThread sample2 = new SampleThread(7);
+            Thread threadOne = new Thread(sample1.run);
+            Thread threadTwo = new Thread(sample2.run);
+            threadOne.Name = "thread one";
+            threadTwo.Name = "thread two";
+            threadOne.IsBackground = true;
+            threadOne.Start();
+            threadTwo.Start();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class SampleThread
+    {
+        private int _count = 0;
+        public SampleThread(int count)
+        {
+            this._count = count;
+        }
+
+        public void run()
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} Runing {i}");
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }
+            Console.WriteLine($"{Thread.CurrentThread.Name} run end");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+    }
+}
