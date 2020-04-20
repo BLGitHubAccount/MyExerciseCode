bb701dfa3adb24fcb602afaac42f1e259e0b385d
baoliangjin <mrbaoliangjin@gmail.com>
2020-04-20 22:47:29 +0800

    3.2在线程池中调用委托 3.3向线程池中放入异步操作

---
+++ Exercise/MultiThreadedLearning/3.2在线程池中调用委托/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+namespace _3._2在线程池中调用委托
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int threadid = 0;
+            RunOnThreadPool poolDelegate = Test;
+            var t = new Thread(()=>Test(out threadid));
+            t.Start();
+            t.Join();
+            Console.WriteLine($"thread id:{threadid}");
+            IAsyncResult r = poolDelegate.BeginInvoke(out threadid,Callback,"a delegate asynchronous call");
+            r.AsyncWaitHandle.WaitOne();
+            string result = poolDelegate.EndInvoke(out threadid,r);
+            Console.WriteLine($"Thread pool worker thread id:{threadid}");
+            Console.WriteLine(result);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            Console.ReadKey();
+        }
+
+        private delegate string RunOnThreadPool(out int threadid);
+
+        private static void Callback(IAsyncResult ar)
+        {
+            Console.WriteLine("stating a callback..");
+            Console.WriteLine($"state passed to a callback:{ar.AsyncState}");
+            Console.WriteLine($"is thread pool thread:{Thread.CurrentThread.IsThreadPoolThread}");
+            Console.WriteLine($"Thread pool worker thread id:{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private static string Test(out int threadid)
+        {
+            Console.WriteLine("Staring..");
+            Console.WriteLine($"is thread pool thread{Thread.CurrentThread.IsThreadPoolThread}");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            threadid = Thread.CurrentThread.ManagedThreadId;
+            return $"Thread pool worker thread id was:{threadid}";
+        }
+    }
+}
---
+++ Exercise/MultiThreadedLearning/3.3向线程池中放入异步操作/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace _3._3向线程池中放入异步操作
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int x = 1;
+            const int y = 2;
+            const string lambdaState = "lambda state 2";
+
+            ThreadPool.QueueUserWorkItem(AsyncOperation);
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+            ThreadPool.QueueUserWorkItem(AsyncOperation,"async state");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                Console.WriteLine($"Operation state:{state}");
+                Console.WriteLine($"worker thread id:{Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            },"lambda state");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            ThreadPool.QueueUserWorkItem(_t =>
+            {
+                Console.WriteLine($"Operation state:{x+y},{lambdaState}");
+                Console.WriteLine($"worker thread id:{Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }, "lambda state");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Console.ReadKey();
+        }
+        private static void AsyncOperation(object state)
+        {
+            Console.WriteLine($"Operation state:{state??"(null)"}");
+            Console.WriteLine($"worker thread id:{Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+    }
+}
