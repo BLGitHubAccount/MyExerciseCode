02d422a83568a3bfabc9fa1c458dfaad3dc0b3f1
baoliangjin <mrbaoliangjin@gmail.com>
2020-04-26 14:45:56 +0800

    zip two lists

---
+++ Exercise/DataStrcAndAlgor/Zip two lists/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zip_two_lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = MakeList(1,2,3,8,9,10);
+            var b = MakeList(4,5,6,7);
+            var head= Zip(a,b);
+            PrintList(head);
+
+            Console.ReadKey();
+        }
+
+        static void PrintList(Node<int> head)
+        {
+            while (head != null)
+            {
+                Console.Write(head.data+"->");
+                head = head.next;
+            }
+
+        }
+
+        static Node<int> Zip(Node<int> p, Node<int> q)
+        {
+            Node<int> head = new Node<int>(), zipper = head;
+            while (true)
+            {
+                if (p == null)
+                {
+                    zipper.next = q;
+                    break;
+                }
+                if (q == null)
+                {
+                    zipper.next = p;
+                    break;
+                }
+                if (p.data < q.data)
+                {
+                    zipper.next = p;
+                    p = p.next;               
+
+                }
+                else
+                {
+                    zipper.next = q;
+                    q = q.next;
+                    
+                }
+                zipper = zipper.next;
+            }
+            return head.next;
+        }
+
+        static Node<int> MakeList(params int[] datas)
+        {
+            Node<int> head = null;
+            for (int i = datas.Length-1; i >=0; i--)
+            {
+                head = new Node<int> { data = datas[i], next = head };
+            }
+            return head;
+        }
+    }
+
+    class Node<T>
+    {
+       public T data;
+       public Node<T> next;
+        public Node()
+        {
+
+        }
+        public Node(T value)
+        {
+            this.data = value;
+        }
+    }
+}
---
+++ Exercise/DataStrcAndAlgor/冒泡排序/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 冒泡排序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] datas = { 1,2,38,6,10,9};
+            int[] results = Short(datas);
+            for (int i = 0; i < results.Length ; i++)
+            {
+                Console.Write(results[i]+"  ");
+            }
+
+            Console.ReadKey();
+        }
+
+       static int[] Short(int[] datas)
+        {
+            //方法1 时间复杂度O(n^2)
+            //for (int i = 0; i < datas.Length-1; i++)
+            //{
+            //    for (int j = i; j < datas.Length - 1; j++)
+            //    {
+            //        if (datas[j] > datas[j + 1])
+            //            swap(datas, j, j + 1);
+            //    }
+
+            //}
+
+            //方法2 时间复杂度O(n^2)
+            for (int i = 0; i < datas.Length; i++)
+            {
+                for (int j = i+1; j < datas.Length; j++)
+                {
+                    if (datas[i] > datas[j])
+                        swap(datas,i,j);                    
+                }
+            }
+            return datas;
+                 
+        }
+              
+            
+
+       static void swap(int[] nums, int i, int j)
+        {
+            //中间变量法
+            int tem = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tem;
+
+            //加减法方式，注意：避免两数相加，大小越界
+            nums[i] += nums[j];
+            nums[j] = nums[i] - nums[j];
+            nums[i] = nums[i] - nums[j];
+            
+
+        }
+    }
+}
